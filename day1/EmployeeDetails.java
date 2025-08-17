2174b841160d08935bf4148189530828ed8ea35d
Prasanna18792 <prasanna18792@gmail.com>
2025-08-17 05:06:37 -0400

    Add files via upload

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,34 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		
+		String name = "John Doe";
+		int age = 30;
+		double salary = 55000.75;
+		char gender	= 'M';
+		boolean	acceptTerms = true;
+		float rating = 4.5f;
+		long mobilenumber = 9876543210L;
+		int pincode = 56001;
+		byte luckynumber = 7;
+		System.out.println("Name :"+name+"\n"+
+		"Age :"+age+"\n"+
+		"Salary :"+salary+"\n"+
+		"Gender :"+gender+"\n"+
+		"Accept Terms :"+acceptTerms+"\n"+
+		"Rating :"+rating+"\n"+
+		"Mobile Number :"+mobilenumber+"\n"+
+		"Pincode :"+pincode+"\n"+
+		"Lucky Number :"+luckynumber
+		
+		);
+		
+				
+				
+		
+
+	}
+
+}
---
+++ day1/FristProgram.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class FristProgram {
+	
+	public static void main(String[] args) {
+		System.out.println("how are you");
+		
+		System.out.println("Hard work never fails"
+				+ "HI");
+	}
+
+	}
+
+
